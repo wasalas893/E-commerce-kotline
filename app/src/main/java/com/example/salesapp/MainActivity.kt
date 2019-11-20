1dcf380dfcd9d7e8688685053dbc0c4b72983bde
wasalas <wasalas893@gmail.com>
2019-11-20 16:46:07 +0530

    create login and singup activity

--- app/src/main/java/com/example/salesapp/MainActivity.kt
+++ app/src/main/java/com/example/salesapp/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.salesapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+       login_signup.setOnClickListener {
+           var i=Intent(this,RegActivity::class.java)
+           startActivity(i)
+       }
+
+
     }
+
+
+
 }
